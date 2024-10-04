20649af1010dce5529071c16e7166fb617f413aa
Loic Nguemegne <loic.nguemegne-temena@polymtl.ca>
2024-10-04 23:51:40 +0000

    lint

--- embedded_ws/robot_script/launch.sh
+++ embedded_ws/robot_script/launch.sh
@@ -7,7 +7,7 @@ else
     cd ~; git clone https://loic-hpp:glpat-v9Qt8vbw2zoY-stynsRH@gitlab.com/polytechnique-montr-al/inf3995/20243/equipe-107/geppetto.git
     cd ~/geppetto/; git switch $MAIN_BRANCH; git pull
 fi
-cd ~/limo_ws; colcon build;  source install/setup.bash; 
+cd ~/limo_ws; colcon build; source install/setup.bash 
 cd ~/limo_ws/src/limo_ros2; ros2 launch limo_bringup limo_start.launch.py &
 cd ~/geppetto/embedded_ws/; colcon build;  source install/setup.bash;  ros2 run com_bridge com_serv &
 source install/setup.bash; ros2 run rosbridge_server rosbridge_websocket
